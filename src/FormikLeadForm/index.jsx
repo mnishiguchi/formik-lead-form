38c66c2509825fda38b27e33ebb070018d48e407
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-12-06 13:17:30 -0500

    Use Formic instead of withFormik HOC

--- src/FormikLeadForm/index.jsx
+++ src/FormikLeadForm/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withFormik } from 'formik';
+import { Formik } from 'formik';
 import _ from 'lodash';
 import Yup from 'yup';
 import {
@@ -14,153 +13,151 @@ import {
 } from 'reactbulma';
 import FormErrorMessage from './FormErrorMessage';
 
-// https://github.com/jaredpalmer/formik
-// https://kulakowka.github.io/react-bulma
-// https://bulma.io/documentation/form/general/
-const InnerForm = props => {
-  const {
-    values,
-    touched,
-    errors,
-    dirty,
-    isSubmitting,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    handleReset
-  } = props;
-
+const FormikLeadForm = () => {
+  const storedValues = JSON.parse(localStorage.getItem('contactForm')) || {};
   return (
-    <form
-      onSubmit={handleSubmit}
-      onKeyPress={event => event.which === 13 && event.preventDefault()}
-    >
-      <Control hasIconsRight>
-        <Input
-          type="name"
-          name="name"
-          placeholder="Name"
-          value={values.name}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          className={errors.name && touched.name ? 'is-danger' : ''}
-        />
-        <Icon right>
-          <i className="fa fa-user" />
-        </Icon>
-      </Control>
+    <Formik
+      displayName="FormikLeadForm"
+      initialValues={{
+        name: storedValues.name || '',
+        email: storedValues.email || '',
+        phone: '',
+        message: '',
+        subscribe: true
+      }}
+      validationSchema={Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string()
+          .email('Invalid email address')
+          .required('Email is required')
+      })}
+      onSubmit={(values, { setSubmitting, setFieldError }) => {
+        // Simulate async request
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          localStorage.setItem(
+            'contactForm',
+            JSON.stringify(_.pick(values, ['name', 'email']))
+          );
 
-      <Control hasIconsRight>
-        <Input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          className={errors.email && touched.email ? 'is-danger' : ''}
-        />
-        <Icon right>
-          <i className="fa fa-envelope" />
-        </Icon>
-      </Control>
+          setSubmitting(false);
+        }, 500);
+      }}
+      render={props => {
+        const {
+          values,
+          touched,
+          errors,
+          dirty,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          handleReset
+        } = props;
 
-      <Control hasIconsRight>
-        <Input
-          type="phone"
-          name="phone"
-          placeholder="Phone"
-          value={values.phone}
-          onChange={handleChange}
-          onBlur={handleBlur}
-          className={errors.phone && touched.phone && 'is-danger'}
-        />
-        <Icon right>
-          <i className="fa fa-phone" />
-        </Icon>
-      </Control>
+        return (
+          <form
+            onSubmit={handleSubmit}
+            onKeyPress={event => event.which === 13 && event.preventDefault()}
+          >
+            <Control hasIconsRight>
+              <Input
+                type="name"
+                name="name"
+                placeholder="Name"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.name && touched.name ? 'is-danger' : ''}
+              />
+              <Icon right>
+                <i className="fa fa-user" />
+              </Icon>
+            </Control>
 
-      <Textarea
-        type="message"
-        name="message"
-        placeholder="Enter your message"
-        value={values.message}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        className={errors.message && touched.message && 'is-danger'}
-        rows={2}
-      />
+            <Control hasIconsRight>
+              <Input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.email && touched.email ? 'is-danger' : ''}
+              />
+              <Icon right>
+                <i className="fa fa-envelope" />
+              </Icon>
+            </Control>
 
-      <Checkbox
-        name="subscribe"
-        value={values.subscribe}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        defaultChecked={true}
-        className={errors.subscribe && touched.subscribe && 'is-danger'}
-      >
-        Update me with the latest apartments and specials.
-      </Checkbox>
+            <Control hasIconsRight>
+              <Input
+                type="phone"
+                name="phone"
+                placeholder="Phone"
+                value={values.phone}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className={errors.phone && touched.phone && 'is-danger'}
+              />
+              <Icon right>
+                <i className="fa fa-phone" />
+              </Icon>
+            </Control>
 
-      {
-        <div style={{ margin: '1rem 0' }}>
-          <FormErrorMessage errors={errors} touched={touched} />
-        </div>
-      }
+            <Textarea
+              type="message"
+              name="message"
+              placeholder="Enter your message"
+              value={values.message}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              className={errors.message && touched.message && 'is-danger'}
+              rows={2}
+            />
 
-      <div>
-        <Button onClick={handleReset} disabled={!dirty || isSubmitting}>
-          Reset
-        </Button>
-        <Button success disabled={isSubmitting}>
-          Contact Property
-        </Button>
-      </div>
+            <Checkbox
+              name="subscribe"
+              value={values.subscribe}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              defaultChecked={true}
+              className={errors.subscribe && touched.subscribe && 'is-danger'}
+            >
+              Update me with the latest apartments and specials.
+            </Checkbox>
 
-      <Notification>
-        <Button
-          onClick={e => {
-            e.preventDefault();
-            localStorage.clear();
-          }}
-        >
-          Clear local storage
-        </Button>
-      </Notification>
-    </form>
+            {
+              <div style={{ margin: '1rem 0' }}>
+                <FormErrorMessage errors={errors} touched={touched} />
+              </div>
+            }
+
+            <div>
+              <Button onClick={handleReset} disabled={!dirty || isSubmitting}>
+                Reset
+              </Button>
+              <Button success disabled={isSubmitting}>
+                Contact Property
+              </Button>
+            </div>
+
+            <Notification>
+              <Button
+                onClick={e => {
+                  e.preventDefault();
+                  localStorage.clear();
+                }}
+              >
+                Clear local storage
+              </Button>
+            </Notification>
+          </form>
+        );
+      }}
+    />
   );
 };
-
-const FormikLeadForm = withFormik({
-  mapPropsToValues: () => {
-    const storedValues = JSON.parse(localStorage.getItem('contactForm')) || {};
-    return {
-      name: storedValues.name || '',
-      email: storedValues.email || '',
-      phone: '',
-      message: '',
-      subscribe: true
-    };
-  },
-  validationSchema: Yup.object().shape({
-    name: Yup.string().required('Name is required'),
-    email: Yup.string()
-      .email('Invalid email address')
-      .required('Email is required')
-  }),
-  handleSubmit: (values, { setSubmitting, setFieldError }) => {
-    // Simulate async request
-    setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
-      localStorage.setItem(
-        'contactForm',
-        JSON.stringify(_.pick(values, ['name', 'email']))
-      );
-
-      setSubmitting(false);
-    }, 500);
-  },
-  displayName: 'FormikLeadForm' // helps with React DevTools
-})(InnerForm);
 
 export default FormikLeadForm;
